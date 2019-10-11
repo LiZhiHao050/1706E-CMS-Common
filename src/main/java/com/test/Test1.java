fa2172d70a9af67268f0f45ab7b7a7f92c2a4e90
LiZhiHao050 <1224120673@qq.com>
2019-10-11 09:38:03 +0800

    第三次提交

--- src/main/java/com/test/Test1.java
+++ src/main/java/com/test/Test1.java
@@ -1,10 +1,13 @@
 package com.test;
 
+import java.util.Date;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("This is Test1");
+		System.out.println("Today is " + new Date());
 		
 	}
 
